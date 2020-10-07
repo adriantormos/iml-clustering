861ddb5a81e819e75a32400947be2c7a220a795d
Adrián Tormos Llorente <adrian.tormos@est.fib.upc.edu>
2020-10-07 12:29:38 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from scipy.io.arff import loadarff
+import pandas as pd
+
+import os
+
+
+def load_arff(dataset_name: str):
+    f = open(os.path.abspath(os.path.join('datasets', 'datasets', dataset_name + '.arff')))
+    data, metadata = loadarff(f)
+    return data, metadata
+
+
+if __name__ == '__main__':
+    data, metadata = load_arff('cmc')
+    data = pd.DataFrame(data)
+    pass
